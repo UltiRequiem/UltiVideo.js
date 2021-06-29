4b9705099a27bd8b5503e14ce4ecc4a5b7091dcf
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-29 14:04:41 -0500

    :art: Update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
-
 const togglePlay = document.querySelector('#togglePlay');
 const toggleMute = document.querySelector('#toggleMute');
 
